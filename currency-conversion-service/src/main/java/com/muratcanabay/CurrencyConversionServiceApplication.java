793e095ede6a6096133378542b078ad1e9170de5
muratcanabay <muratcanabay@hotmail.com>
2022-04-16 12:05:48 +0300

    currency-conversion-service added

--- currency-conversion-service/src/main/java/com/muratcanabay/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/muratcanabay/CurrencyConversionServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyServiceApplication {
+public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CurrencyServiceApplication.class, args);
+        SpringApplication.run(CurrencyConversionServiceApplication.class, args);
     }
 }
