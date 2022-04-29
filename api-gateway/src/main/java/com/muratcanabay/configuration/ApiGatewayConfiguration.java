88c5b565dde396c20e6cb68aa3840bc5ea488724
muratcanabay <muratcanabay@hotmail.com>
2022-04-29 15:23:51 +0300

    gateway api router

---
+++ api-gateway/src/main/java/com/muratcanabay/configuration/ApiGatewayConfiguration.java
@@ -0,0 +1,23 @@
+package com.muratcanabay.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    /**
+     * {@link RouteLocator} works default as following: {@code builder.routes().build()}
+     *
+     * @return {@link RouteLocator}
+     */
+    @Bean
+    public RouteLocator route(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange-service"))
+                .route(p -> p.path("/currency-conversion/**").uri("lb://currency-conversion-service"))
+                .build();
+    }
+}
